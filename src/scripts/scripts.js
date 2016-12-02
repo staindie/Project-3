0cffb6097884088f7dae008b676329a93e131293
Alexander Dikusar <a.dikusar@softteco.com>
2016-12-02 18:37:08 +0300

    Changes

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,9 +1,34 @@
-//(function () {
-//    var app = angular.module('myApp', ['ngDialog']);
-//
-//    app.controller('MainController',['$scope', function ($scope) {
-//        console.log($scope);
-//    }]);
-//
-//
-//})();
+(function () {
+    var app = angular.module('myApp', ['ngDialog', 'kendo.directives']);
+
+    app.controller('MainController',['$scope', function ($scope) {
+
+        $scope.dataSource = new kendo.data.PivotDataSource({
+            type: "xmla",
+            columns: [{ name: "[Date].[Calendar]", expand: true }, { name: "[Product].[Category]" } ],
+            rows: [{ name: "[Geography].[City]" }],
+            measures: ["[Measures].[Reseller Freight Cost]"],
+            transport: {
+                connection: {
+                    catalog: "Adventure Works DW 2008R2",
+                    cube: "Adventure Works"
+                },
+                read: "//demos.telerik.com/olap/msmdpump.dll"
+            },
+            schema: {
+                type: "xmla"
+            },
+            error: function (e) {
+                alert("error: " + kendo.stringify(e.errors[0]));
+            }
+        });
+        $scope.options = {
+            columnWidth: 200,
+            height: 580,
+            dataSource: $scope.dataSource
+        };
+
+    }]);
+
+
+})();
