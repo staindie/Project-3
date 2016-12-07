9360c72117167899d7bb88ae293c877d2a2f7a17
Alexander Dikusar <e.pavlova@softteco.com>
2016-12-07 20:30:05 +0300

    Grid made

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -21,7 +21,7 @@
                                 "chart": {
                                     "caption": "Monthly revenue for last year",
                                     "subCaption": "Harry's SuperMart",
-                                    "xAxisName": "Month",
+                                    "xAxisName": "Year",
                                     "yAxisName": "Revenues (In USD)",
                                     "theme": "fint"
                                 },
@@ -40,39 +40,68 @@
     app.controller('MainController',['$scope', function ($scope) {
 
         $scope.chartObject = [
-            {"label": "Jan", "value": "420000"},
-            {"label": "Feb", "value": "810000"},
-            {"label": "Mar", "value": "720000"},
-            {"label": "Apr", "value": "550000"},
-            {"label": "May", "value": "910000"},
-            {"label": "Jun", "value": "510000"},
-            {"label": "Jul", "value": "680000"},
-            {"label": "Aug", "value": "620000"},
-            {"label": "Sep", "value": "610000"},
-            {"label": "Oct", "value": "490000"},
-            {"label": "Nov", "value": "900000"},
-            {"label": "Dec", "value": "730000"}
+            {"ProductName": "2010", "value": 42, "value-2": 10, Category: "Beverages"},
+            {"ProductName": "2010", "value": 42, "value-2": 20, Category: "Food"},
+            {"ProductName": "2011", "value": 81, "value-2": 30, Category: "Neverages"},
+            {"ProductName": "2012", "value": 72, "value-2": 40, Category: "Beverages"},
+            {"ProductName": "2013", "value": 55, "value-2": 60, Category: "Neverages"},
+            {"ProductName": "2014", "value": 91, "value-2": 70, Category: "Food"},
+            {"ProductName": "2015", "value": 51, "value-2": 80, Category: "Neverages"}
         ];
 
         $scope.dataSource = new kendo.data.PivotDataSource({
-            type: "xmla",
-            columns: [{ name: "[Date].[Calendar]", expand: true }, { name: "[Product].[Category]" } ],
-            rows: [{ name: "[Geography].[City]" }],
-            measures: ["[Measures].[Reseller Freight Cost]"],
-            transport: {
-                connection: {
-                    catalog: "Adventure Works DW 2008R2",
-                    cube: "Adventure Works"
-                },
-                read: "//demos.telerik.com/olap/msmdpump.dll"
-            },
+            columns: ["ProductName" ],
+            rows: ["Category"],
+            measures: ["Sum"],
+            data: $scope.chartObject,
             schema: {
-                type: "xmla"
-            },
-            error: function (e) {
-                alert("error: " + kendo.stringify(e.errors[0]));
+                cube: {
+                    dimensions: {
+                        ProductName: {caption: "All Products"},
+                        Category: {caption: "All Categories"}
+                    },
+                    measures: {
+                        "Sum": {
+                            field: "value",
+                            format: "{0:c}",
+                            aggregate: function (value, state, context) {
+                                return (state.accumulator || 0) + value;
+                            }
+                        },
+                        "Sum-2": {
+                            field: "value-2",
+                            format: "{0:c}",
+                            aggregate: function (value, state, context) {
+                                return (state.accumulator || 0) + value;
+                            }
+                        }
+                    }
+                }
             }
         });
+
+        console.log($scope.dataSource);
+
+        //$scope.dataSource = new kendo.data.PivotDataSource({
+        //    type: "xmla",
+        //    columns: [{ name: "[Date].[Calendar]", expand: true }, { name: "[Product].[Category]" } ],
+        //    rows: [{ name: "[Geography].[City]" }],
+        //    measures: ["[Measures].[Reseller Freight Cost]"],
+        //    transport: {
+        //        connection: {
+        //            catalog: "Adventure Works DW 2008R2",
+        //            cube: "Adventure Works"
+        //        },
+        //        read: "//demos.telerik.com/olap/msmdpump.dll"
+        //    },
+        //    schema: {
+        //        type: "xmla"
+        //    },
+        //    error: function (e) {
+        //        alert("error: " + kendo.stringify(e.errors[0]));
+        //    }
+        //});
+
         $scope.options = {
             columnWidth: 200,
             height: 580,
