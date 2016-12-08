1a08a9a8895fe7c6304aa16a204b8f58046a478d
Alexander Dikusar <e.pavlova@softteco.com>
2016-12-08 20:04:52 +0300

    Grid made

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -12,20 +12,72 @@
 
                     FusionCharts.ready(function(){
                         var revenueChart = new FusionCharts({
-                            "type": "column2d",
+                            "type": "mscolumn3d",
                             "renderAt": $scope.containerName,
                             "width": "500",
                             "height": "300",
                             "dataFormat": "json",
                             "dataSource": {
                                 "chart": {
-                                    "caption": "Monthly revenue for last year",
-                                    "subCaption": "Harry's SuperMart",
+                                    "caption": "Revenue",
                                     "xAxisName": "Year",
                                     "yAxisName": "Revenues (In USD)",
                                     "theme": "fint"
                                 },
-                                "data": $scope.chartObject
+                                "categories": [
+                                    {
+                                        "category": [
+                                            {
+                                                "label": "Quarter 1"
+                                            },
+                                            {
+                                                "label": "Quarter 2"
+                                            },
+                                            {
+                                                "label": "Quarter 3"
+                                            },
+                                            {
+                                                "label": "Quarter 4"
+                                            }
+                                        ]
+                                    }
+                                ],
+                                "dataset": [
+                                    {
+                                        "seriesname": "Fixed Cost",
+                                        "data": [
+                                            {
+                                                "value": "235000"
+                                            },
+                                            {
+                                                "value": "225100"
+                                            },
+                                            {
+                                                "value": "222000"
+                                            },
+                                            {
+                                                "value": "230500"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "seriesname": "Variable Cost",
+                                        "data": [
+                                            {
+                                                "value": "230000"
+                                            },
+                                            {
+                                                "value": "143000"
+                                            },
+                                            {
+                                                "value": "198000"
+                                            },
+                                            {
+                                                "value": "327600"
+                                            }
+                                        ]
+                                    }
+                                ]
                             }
                         });
 
@@ -39,68 +91,47 @@
 
     app.controller('MainController',['$scope', function ($scope) {
 
-        $scope.chartObject = [
-            {"ProductName": "2010", "value": 42, "value-2": 10, Category: "Beverages"},
-            {"ProductName": "2010", "value": 42, "value-2": 20, Category: "Food"},
-            {"ProductName": "2011", "value": 81, "value-2": 30, Category: "Neverages"},
-            {"ProductName": "2012", "value": 72, "value-2": 40, Category: "Beverages"},
-            {"ProductName": "2013", "value": 55, "value-2": 60, Category: "Neverages"},
-            {"ProductName": "2014", "value": 91, "value-2": 70, Category: "Food"},
-            {"ProductName": "2015", "value": 51, "value-2": 80, Category: "Neverages"}
+        $scope.dataObject = [
+            {"Year": "2010", "value": 42, "value2": 15, Category: "Beverages"},
+            {"Year": "2010", "value": 42, "value2": 2, Category: "Food"},
+            {"Year": "2011", "value": 81, "value2": 3, Category: "Neverages"},
+            {"Year": "2012", "value": 72, "value2": 4, Category: "Beverages"},
+            {"Year": "2013", "value": 55, "value2": 6, Category: "Neverages"},
+            {"Year": "2014", "value": 91, "value2": 7, Category: "Food"},
+            {"Year": "2015", "value": 51, "value2": 8, Category: "Neverages"}
         ];
 
         $scope.dataSource = new kendo.data.PivotDataSource({
-            columns: ["ProductName" ],
-            rows: ["Category"],
-            measures: ["Sum"],
-            data: $scope.chartObject,
+            columns: ["Year" ],
+            rows: [{name: "Category", expand: true}],
+            measures: [{name: 'Sum2'}, {name: 'Sum3'}],
+            data: $scope.dataObject,
             schema: {
                 cube: {
                     dimensions: {
-                        ProductName: {caption: "All Products"},
+                        Year: {caption: "All years"},
                         Category: {caption: "All Categories"}
                     },
                     measures: {
                         "Sum": {
                             field: "value",
                             format: "{0:c}",
-                            aggregate: function (value, state, context) {
-                                return (state.accumulator || 0) + value;
-                            }
+                            aggregate: "sum"
                         },
-                        "Sum-2": {
-                            field: "value-2",
-                            format: "{0:c}",
-                            aggregate: function (value, state, context) {
-                                return (state.accumulator || 0) + value;
-                            }
+                        "Sum2": {
+                            field: "value2",
+                            format: "{0:n}",
+                            aggregate: "sum"
+                        },
+                        "Sum3": {
+                            field: "value2*1/20",
+                            format: "{0:p}",
+                            aggregate: "average"
                         }
                     }
                 }
             }
         });
-
-        console.log($scope.dataSource);
-
-        //$scope.dataSource = new kendo.data.PivotDataSource({
-        //    type: "xmla",
-        //    columns: [{ name: "[Date].[Calendar]", expand: true }, { name: "[Product].[Category]" } ],
-        //    rows: [{ name: "[Geography].[City]" }],
-        //    measures: ["[Measures].[Reseller Freight Cost]"],
-        //    transport: {
-        //        connection: {
-        //            catalog: "Adventure Works DW 2008R2",
-        //            cube: "Adventure Works"
-        //        },
-        //        read: "//demos.telerik.com/olap/msmdpump.dll"
-        //    },
-        //    schema: {
-        //        type: "xmla"
-        //    },
-        //    error: function (e) {
-        //        alert("error: " + kendo.stringify(e.errors[0]));
-        //    }
-        //});
 
         $scope.options = {
             columnWidth: 200,
@@ -108,6 +139,27 @@
             dataSource: $scope.dataSource
         };
 
+        $scope.showScope = function() {
+            console.log($scope);
+        }
+
+        $scope.getChartObject = function() {
+            var rowNamesArray = [];
+            var upperHeadersNamesArray = [];
+            var lowerHeadersNamesArray = [];
+            $.each($('.k-grid.k-widget.k-alt tbody td:nth-child(2) span'), function (index, item) {
+                rowNamesArray.push(item.innerHTML);
+            });
+            $.each($('.k-grid-header-wrap tr:nth-child(2) span'), function (index, item) {
+                upperHeadersNamesArray.push(item.innerHTML);
+            });
+            $.each($('.k-grid-header-wrap tr:nth-child(3) span'), function (index, item) {
+                if (lowerHeadersNamesArray.indexOf(item.innerHTML) == -1){
+                    lowerHeadersNamesArray.push(item.innerHTML);
+                }
+            });
+            console.log(lowerHeadersNamesArray);
+        }
 
     }]);
 
