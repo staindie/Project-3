96bd88f949f1472ab2dd21c6de49d31cb510e7cc
Alexander Dikusar <e.pavlova@softteco.com>
2016-12-13 13:39:52 +0300

    Some changes

---
+++ src/scripts/chart-scripts.js
@@ -0,0 +1,159 @@
+function flattenTree(tuples) {
+    tuples = tuples.slice();
+    var result = [];
+    var tuple = tuples.shift();
+    var idx, length, spliceIndex, children, member;
+
+    while (tuple) {
+        //required for multiple measures
+        if(tuple.dataIndex !== undefined) {
+            result.push(tuple);
+        }
+
+        spliceIndex = 0;
+        for (idx = 0, length = tuple.members.length; idx < length; idx++) {
+            member = tuple.members[idx];
+            children = member.children;
+            if (member.measure) {
+                [].splice.apply(tuples, [0, 0].concat(expandMeasures(children, tuple)));
+            } else {
+                [].splice.apply(tuples, [spliceIndex, 0].concat(children));
+            }
+            spliceIndex += children.length;
+        }
+
+        tuple = tuples.shift();
+    }
+
+    return result;
+}
+
+function clone(tuple, dataIndex) {
+    var members = tuple.members.slice();
+
+    return {
+        dataIndex: dataIndex,
+        members: $.map(members, function(m) {
+            return $.extend({}, m, { children: [] });
+        })
+    };
+}
+
+function replaceLastMember(tuple, member) {
+    tuple.members[tuple.members.length - 1] = member;
+    return tuple;
+}
+
+function expandMeasures(measures, tuple) {
+    return $.map(measures, function(measure) {
+        return replaceLastMember(clone(tuple, measure.dataIndex), measure);
+    });
+}
+
+//Check whether the tuple has been collapsed
+function isCollapsed(tuple, collapsed) {
+    var members = tuple.members;
+
+    for (var idx = 0, length = collapsed.length; idx < length; idx++) {
+        var collapsedPath = collapsed[idx];
+        if (indexOfPath(fullPath(members, collapsedPath.length -1), [collapsedPath]) !== -1) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//Work with tuple names/captions
+function indexOfPath(path, paths) {
+    var path = path.join(",");
+
+    for (var idx = 0; idx < paths.length; idx++) {
+        if (paths[idx].join(",") === path) {
+            return idx;
+        }
+    }
+
+    return -1;
+}
+
+function fullPath(members, idx) {
+    var path = [];
+    var parentName = members[idx].parentName;
+
+    idx -= 1;
+
+    while (idx > -1) {
+        path.push(members[idx].name);
+        idx -= 1;
+    }
+
+    path.push(parentName);
+
+    return path;
+}
+
+function memberCaption(member) { return member.caption }
+
+function extractCaption(members) {
+    return $.map(members, memberCaption).join(" ");
+}
+
+function fullPathCaptionName(members, dLength, idx) {
+    var result = extractCaption(members.slice(0, idx + 1));
+    var measureName = extractCaption(members.slice(dLength, members.mLength));
+
+    if (measureName) {
+        result += " - " + measureName;
+    }
+
+    return result;
+}
+
+//the main function that convert PivotDataSource data into understandable for the Chart widget format
+function convertData(dataSource, collapsed) {
+    var columnDimensionsLength = dataSource.columns().length;
+    var rowDimensionsLength = dataSource.rows().length;
+
+    var columnTuples = flattenTree(dataSource.axes().columns.tuples || [], collapsed.columns);
+    var rowTuples = flattenTree(dataSource.axes().rows.tuples || [], collapsed.rows);
+    var data = dataSource.data();
+    var rowTuple, columnTuple;
+
+    var idx = 0;
+    var result = [];
+    var columnsLength = columnTuples.length;
+
+    for (var i = 0; i < rowTuples.length; i++) {
+        rowTuple = rowTuples[i];
+
+        if (!isCollapsed(rowTuple, collapsed.rows)) {
+            for (var j = 0; j < columnsLength; j++) {
+                columnTuple = columnTuples[j];
+
+                if (!isCollapsed(columnTuple, collapsed.columns)) {
+                    if (idx > columnsLength && idx % columnsLength !== 0) {
+                        for (var ri = 0; ri < rowTuple.members.length; ri++) {
+                            for (var ci = 0; ci < columnTuple.members.length; ci++) {
+                                //do not add root tuple members, e.g. [CY 2005, All Employees]
+                                //Add only children
+                                if (!columnTuple.members[ci].parentName || !rowTuple.members[ri].parentName) {
+                                    continue;
+                                }
+
+                                result.push({
+                                    measure: Number(data[idx].value),
+                                    column: fullPathCaptionName(columnTuple.members, columnDimensionsLength, ci),
+                                    row: fullPathCaptionName(rowTuple.members, rowDimensionsLength, ri)
+                                });
+                            }
+                        }
+                    }
+                }
+                idx += 1;
+            }
+        }
+    }
+
+    return result;
+}
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,56 +1,5 @@
 $(document).ready(function () {
 
-    //chartObject.chart = {
-    //    "caption": "Revenue",
-    //    "xAxisName": "Year",
-    //    "yAxisName": "Revenues (In USD)",
-    //    "theme": "fint"
-    //};
-
-    //var chartObject = {
-    //    "categories": [
-    //        {
-    //            "category": [{"label": "Jan"}, {"label": "Feb"}, {"label": "Mar"}, {"label": "Apr"}, {"label": "May"}, {"label": "Jun"}, {"label": "Jul"}, {"label": "Aug"}, {"label": "Sep"}, {"label": "Oct"}, {"label": "Nov"}, {"label": "Dec"}]
-    //        }
-    //    ],
-    //    "dataset": [
-    //        {
-    //            "seriesname": "2015",
-    //            "data": [{"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}]
-    //        },
-    //        {
-    //            "seriesname": "2014",
-    //            "data": [{"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}]
-    //        },
-    //        {
-    //            "seriesname": "2013",
-    //            "data": [{"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}, {"value": "12200"}]
-    //        }
-    //    ]
-    //};
-    //
-    //var createChart = function () {
-    //
-    //    console.log(chartObject);
-    //    console.log($scope.chartObject);
-    //
-    //    FusionCharts.ready(function () {
-    //        var revenueChart = new FusionCharts({
-    //            "type": "mscolumn3d",
-    //            "renderAt": $scope.containerName,
-    //            "width": "80%",
-    //            "height": "500",
-    //            "dataFormat": "json",
-    //            //"dataSource": chartObject
-    //            "dataSource": $scope.chartObject
-    //        });
-    //        revenueChart.render();
-    //    });
-    //};
-    //
-    //createChart();
-
-
     var dataObject = [
         {"Year": "2010", "value": 42, "value2": 15, Category: "Beverages"},
         {"Year": "2010", "value": 42, "value2": 2, Category: "Food"},
@@ -65,7 +14,7 @@ $(document).ready(function () {
         columns: [{name: "Year", expand: true}],
         rows: [{name: "Category", expand: true}],
         measures: [{name: 'Sum2'}, {name: 'Sum3'}],
-        data: $scope.dataObject,
+        data: dataObject,
         schema: {
             cube: {
                 dimensions: {
@@ -93,48 +42,86 @@ $(document).ready(function () {
         }
     });
 
-    var options = {
-        columnWidth: 200,
-        height: 580,
-        dataSource: $scope.dataSource,
-        dataBound: function (e) {
-            $scope.getChartObject();
-        }
+    var collapsed = {
+        columns: [],
+        rows: []
     };
 
-    var getChartObject = function () {
-        var rowNamesArray = [];
-        var allHeadersNamesArray = [];
-        var category = [];
-        var dataSet = [];
+    var pivotgrid = $("#pivotgrid").kendoPivotGrid({
+        filterable: true,
+        //gather the collapsed members
+        collapseMember: function(e) {
+            var axis = collapsed[e.axis];
+            var path = e.path;
 
-        $.each($('.k-grid.k-widget.k-alt tbody td:last-child span'), function (index, item) {
-            rowNamesArray.push(item.innerHTML);
-        });
-
-        $.each($('.k-grid-header-wrap tr:nth-child(2) th'), function (index, item) {
-            var colspan = $(item).attr('colspan');
-            var upperText = $(item).find('span').html();
-            for (var i = 1; i <= colspan; i++) {
-                var lowerText = upperText + ' ' + $('.k-grid-header-wrap tr:nth-child(3) th:nth-child(' + i + ') span').html();
-                allHeadersNamesArray.push(lowerText);
+            if (indexOfPath(path, axis) === -1) {
+                axis.push(path);
             }
-        });
+        },
+        //gather the expanded members
+        expandMember: function(e) {
+            var axis = collapsed[e.axis];
+            var index = indexOfPath(e.path, axis);
 
-        rowNamesArray.forEach(function (item, index) {
-            dataSet.push({seriesname: item, data: []});
-        });
+            if (index !== -1) {
+                axis.splice(index, 1);
+            }
+        },
+        columnWidth: 100,
+        height: 630,
+        dataSource: dataSource,
+        dataBound: function() {
+            //create/bind the chart widget
+            initChart(convertData(this.dataSource, collapsed));
+        }
+    }).data("kendoPivotGrid");
 
-        allHeadersNamesArray.forEach(function (item, index_col) {
-            category.push({label: item});
-            rowNamesArray.forEach(function (item, index_row) {
-                var val = $('.k-grid-content tr:nth-child(' + (index_row + 1) + ') td:nth-child(' + (index_col + 1) + ') ').html();
-                console.log(val);
-                dataSet[index_row].data.push({value: val});
+    $("#configurator").kendoPivotConfigurator({
+        dataSource: dataSource,
+        filterable: true,
+        sortable: true,
+        columnWidth: '50%',
+        height: 630
+    });
+
+    function initChart(data) {
+        var chart = $("#chart").data("kendoChart");
+
+        if (!chart) {
+            $("#chart").kendoChart({
+                dataSource: {
+                    data: data,
+                    group: "row"
+                },
+                series: [{
+                    type: "column",
+                    field: "measure",
+                    name: "#= group.value # (category)",
+                    categoryField: "column"
+                }],
+                legend: {
+                    position: "bottom"
+                },
+                categoryAxis: {
+                    labels: {
+                        rotation: 310
+                    }
+                },
+                valueAxis: {
+                    labels: {
+                        format: "${0}"
+                    }
+                },
+                dataBound: function(e) {
+                    var categoryAxis = e.sender.options.categoryAxis;
+
+                    if (categoryAxis && categoryAxis.categories) {
+                        categoryAxis.categories.sort();
+                    }
+                }
             });
-        });
-
-        dataObject = {categories: [{category: category}], dataset: dataSet};
-    };
-
+        } else {
+            chart.dataSource.data(data);
+        }
+    }
 });
